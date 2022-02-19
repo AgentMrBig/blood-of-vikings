569604f9055cfaaf10aa0f7df803b96834da1497
AgentMrBig <agentmrbig@gmail.com>
2022-02-19 09:19:48 -0500

    setup chec commerce.js, pulling product data from chec api and displaying products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,12 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$1.10', image: 'https://picsum.photos/200' },
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$5.10',image: 'https://picsum.photos/300'  }
-]
+// const products = [
+//     { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$1.10', image: 'https://picsum.photos/200' },
+//     { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$5.10',image: 'https://picsum.photos/300'  }
+// ]
 
-const Products = () => {
+const Products = ({products}) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
