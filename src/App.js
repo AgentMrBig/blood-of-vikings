7197fe46f0a0a256855c12e4ba174702010638e8
AgentMrBig <agentmrbig@gmail.com>
2022-02-19 00:15:18 -0500

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import Products from './components/Products/Products';
+
+import {Products, Navbar} from './Components';
 
 const App = () => {
   return (
+    <div>
+      <Navbar />
     <Products />
+    </div>
   )
 }
 
